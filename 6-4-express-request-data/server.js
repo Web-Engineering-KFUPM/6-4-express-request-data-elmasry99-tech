b4f30106f1aa16d16d449e7b30c548dfe47292d7
Mazen Osama <elkafrawy@MacBook-Air-Mazen.local>
2026-04-08 14:27:40 +0300

    Second to do

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -134,3 +134,12 @@ const app= express();
 app.listen(3000,()=>{
     console.log("API running at http://localhost:3000");
 });
+
+app.get("/echo",(req,res)=>{
+   const {name, age} = req.query;
+   if(!name || !age){
+    return res.status(400).json({ok:false, error:"name & age required"});
+   }
+   res.json({ok:true, name, age, msg:"Hello ${name}, you are ${age}"});
+});
+
